7df882dbb9764721812547a1d16ba06bf13ffdbf
themonster2015 <quynhyen.vothi@gmail.com>
2021-11-11 16:59:28 +0100

    write tests for destination model

--- spec/models/destination_spec.rb
+++ spec/models/destination_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Destination, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    subject  {Destination.create(name: "London", location: 'London', price_per_day: 25, image_url: 'https://via.placeholder.com/150')}
+
+    it 'create a valid destination object' do
+        expect(subject).to be_valid
+    end
+  
+    describe 'validations' do
+        it { should validate_presence_of(:name) }
+        it { should validate_presence_of(:location) }
+        it { should validate_presence_of(:image_url) }
+        it { should validate_presence_of(:price_per_day) }
+      end
+    
+      describe 'associations' do
+        it { should have_many(:reservations) }
+        it { should have_many(:users).through(:reservations) }
+  
+      end
 end
