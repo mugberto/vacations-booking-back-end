1a9374cc60bd4094ce0ab39fe70612641db36caf
Hubert MUGABO <mugberto@gmail.com>
2021-11-11 16:03:21 +0200

    Add validations and associations to Destination model

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,2 +1,11 @@
 class Destination < ApplicationRecord
+
+  has_many :reservations
+  has_many :users, through: :reservations
+
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :location, presence: true, length: { maximum: 100 }
+  validates :image_url, presence: true
+  validates :price_per_day, presence: true, numericality: true
+  validates_association :reservations
 end
