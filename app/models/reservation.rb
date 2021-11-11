f77b8d6a8571013c1be673c95026f65de0a85add
Igor Koloski <koloski_igor@hotmail.com>
2021-11-11 22:01:29 +0100

    Add endpoints for reservations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,6 +4,6 @@ class Reservation < ApplicationRecord
 
   validates :user_id, presence: true
   validates :destination_id, presence: true
-  validates :startingDay, presence: true
-  validates :endingDay, presence: true
+  # validates :startingDay, presence: true
+  # validates :endingDay, presence: true
 end
