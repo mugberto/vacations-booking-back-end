5432938583a17aed11e342a06c21a7e110c74eee
Igor Koloski <koloski_igor@hotmail.com>
2021-11-11 22:08:56 +0100

    Add comment to the model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,6 +4,7 @@ class Reservation < ApplicationRecord
 
   validates :user_id, presence: true
   validates :destination_id, presence: true
+  # commented for testing purposes
   # validates :startingDay, presence: true
   # validates :endingDay, presence: true
 end
