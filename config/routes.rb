bd8a552ece213eaa07b8ddafb2d459146f6a4de6
Aleksandar <a.ivezic@yahoo.com>
2021-11-12 14:15:10 +0100

    Configure JWT and Devise and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   end
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "reservations#index"
+  # root to: "reservations#index"
   # GET /reservations (gets all reservations)
   # POST /reservations (the id's are hardcoded but the postman post method works if you send dates as json data
   #{ "startingDay": "smth", "endingDay": "smth" }
