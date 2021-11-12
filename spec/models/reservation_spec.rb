3164626ae166359d66d3d598796d6d35b968912f
themonster2015 <quynhyen.vothi@gmail.com>
2021-11-12 09:25:26 +0100

    fix rubocop errors

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
-
   describe 'validations' do
     it { should validate_presence_of(:user_id) }
     it { should validate_presence_of(:destination_id) }
