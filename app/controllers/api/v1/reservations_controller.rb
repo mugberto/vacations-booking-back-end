08ac8f84539fbd85a242ee3a776261ddabb59432
Aleksandar <a.ivezic@yahoo.com>
2021-11-12 14:24:04 +0100

    Refactor reservations_controller

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,7 +1,7 @@
-class ReservationsController < ApplicationController
+class Api::V1::ReservationsController < ApiController
   def index
     @reservations = Reservation.all
-    # respond_to do |format|
+    # respond_to do |format|authenticate_user!
     #   format.html # index.html.erb
     #   format.xml { render xml: @reservations }
     #   format.json { render json: @reservations }
@@ -10,25 +10,31 @@ class ReservationsController < ApplicationController
 
   def create
     @reservation = Reservation.new(reservation_params)
-    @reservation.destination_id = 1 # params[:destination_id]
-    @reservation.user = User.first # current_user
+    # @reservation.destination_id = params[:destination_id] (will be needed if id is in )
+    @token = request.headers.fetch('Authorization', '').split.last
+    @user_id = JsonWebToken.decode(@token).first
+    @reservation.user_id = User.find(@user_id.last)
+    # @reservation.destination_id = 1 # params[:destination_id]
+    # @reservation.user = User.first # current_user
+
+    # @reservation = current_user.reservations.build(reservation_params)
 
     if @reservation.save
-      redirect_to :root, notice: 'Reservation was successfully created.'
+      render json: { reservation: @reservation }
     else
-      redirect_to :root, alert: @reservation.errors.full_messages.join('. ').to_s
+      render json: { errors: @reservation.errors }
     end
   end
 
   def destroy
     @reservation = Reservation.find(params[:id])
     @reservation.destroy
-    redirect_to :root, notice: 'Your reservation has been deleted'
+    render json: { message: 'Reservation succesfully removed' }
   end
 
   private
 
   def reservation_params
-    params.require(:reservation).permit(:startingDay, :endingDay)
+    params.require(:reservation).permit(:destination_id, :startingDay, :endingDay)
   end
 end
