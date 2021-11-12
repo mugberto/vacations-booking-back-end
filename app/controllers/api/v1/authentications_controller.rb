54bffe4e3a248f8f34bb1e41c7f9917beeef41c8
Aleksandar <a.ivezic@yahoo.com>
2021-11-12 14:13:20 +0100

    Refactor Authentication controller

--- app/controllers/api/v1/authentications_controller.rb
+++ app/controllers/api/v1/authentications_controller.rb
@@ -1,5 +1,12 @@
-class Api::v1::AuthenticationsController < ApplicationController
+class Api::V1::AuthenticationsController < ApiController
+  skip_before_action :authenticate_user!
   def create
     @user = User.find_by(email: params[:email])
+
+    if @user.valid_password? params[:password]
+      render json: { token: JsonWebToken.encode(sub: @user.id) }
+    else
+      render json: { errors: ['Invalid email or password'] }
+    end
   end
 end
