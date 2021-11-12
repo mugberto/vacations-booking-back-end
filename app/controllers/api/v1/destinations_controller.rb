356941e9911020253203a6888535d17afb0c579f
Hubert MUGABO <mugberto@gmail.com>
2021-11-12 03:04:18 +0200

    Add endpoint for creating a destination

--- app/controllers/api/v1/destinations_controller.rb
+++ app/controllers/api/v1/destinations_controller.rb
@@ -7,18 +7,19 @@ class Api::V1::DestinationsController < ApplicationController
     @destination = Destination.find(params[:id])
   end
 
-  # def create
-  #   Destination.create!(dest_params)
-  # rescue Exception => e
-  #   render :
-  # end
+  def create
+    Destination.create!(dest_params)
+    render json: { status: 'Success!', message: 'Destination created successively' }
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { status: 'Error!', message: e.record.errors }
+  end
 
   # def destroy
   #   @destination = Destination.find(params[:id])
   #   @destination.destroy
   # end
 
-  # def dest_params
-  #   params.permit(:name, :location, :image_url, :price_per_day)
-  # end
+  def dest_params
+    params.permit(:name, :location, :image_url, :price_per_day)
+  end
 end
