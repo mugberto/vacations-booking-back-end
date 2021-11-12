50ec98bce149dce90d9e6ceb8359de5daf366b7d
themonster2015 <quynhyen.vothi@gmail.com>
2021-11-12 09:23:47 +0100

    remove unnecessary code in reservation test

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,20 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
-  before do
-    @user = User.create(email: 'a@b.c', username: 'user1', password: '123456', password_confirmation: '123456')
-    @destination = Destination.create(name: 'London', location: 'London', price_per_day: 25,
-                                      image_url: 'https://via.placeholder.com/150')
-  end
-
-  subject do
-    Reservation.create(user_id: @user.id, destination_id: @destination.id, startingDay: '01/01/2015',
-                       endingDay: '01/01/2015')
-  end
-
-  it 'create a valid destination object' do
-    expect(subject).to be_valid
-  end
 
   describe 'validations' do
     it { should validate_presence_of(:user_id) }
