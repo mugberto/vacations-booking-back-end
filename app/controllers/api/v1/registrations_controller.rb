42e3a6d7dacf76cf496797b02acd81ca68d65e55
Aleksandar <a.ivezic@yahoo.com>
2021-11-12 14:13:52 +0100

    Refactor registration controller

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,14 +1,16 @@
-class Api::V1::RegistrationsController < ApplicationController
-  before_action :create_user
+class Api::V1::RegistrationsController < ApiController
+  skip_before_action :authenticate_user!
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: { token: JsonWebToken.encode(sub: @user.id) }
+    else
+      render json: { errors: 'Registration failed!' }
+    end
   end
 
   private
-
-  def create_user
-    @user = User.new(user_params)
-  end
 
   def user_params
     params.permit(:name, :email, :password, :password_confirmation)
