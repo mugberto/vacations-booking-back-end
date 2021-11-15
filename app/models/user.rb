c2b90e5de82334a7bde9accb5c1382e1e16f517e
Aleksandar <a.ivezic@yahoo.com>
2021-11-15 20:00:42 +0100

    Add password validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,6 @@ class User < ApplicationRecord
 
   validates :username, presence: true
   validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
 end
