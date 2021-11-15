3474aae92f6e09a69f8bfd7be93507fb318c2bf6
Aleksandar <a.ivezic@yahoo.com>
2021-11-15 20:01:23 +0100

    Update user_model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should have_many(:reservations) }
+    it { should have_many(:destinations) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
+    it { should validate_presence_of(:password_confirmation) }
+  end
 end
