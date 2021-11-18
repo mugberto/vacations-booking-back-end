19861634a636174128b036a471a2c8ac7887b8c2
Aleksandar <a.ivezic@yahoo.com>
2021-11-18 15:19:31 +0100

    Created total_cost method (Reservation) and added to API response

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -6,4 +6,9 @@ class Reservation < ApplicationRecord
   validates :destination_id, presence: true
   validates :startingDay, presence: true
   validates :endingDay, presence: true
+
+  def total_cost
+    @days = (self.endingDay - self.startingDay).to_i
+    @days * self.destination.price_per_day.to_i    
+  end
 end
