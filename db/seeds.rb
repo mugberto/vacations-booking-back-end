74a7343037d82abcf7667b1497a94b5229a127af
Aleksandar Ivezic <a.ivezic@yahoo.com>
2022-07-25 14:23:01 +0200

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,9 @@ User.destroy_all
 Reservation.destroy_all
 
 puts("Seed start...")
-Destination.create(name: 'Bora Bora', location: 'French Polynesia', image_url: 'https://res.cloudinary.com/dqdrsilxz/image/upload/v1636638534/bora-bora-island_tcyoev.jpg', price_per_day: 150)
-Destination.create(name: 'Rome', location: 'Italy', image_url: 'https://res.cloudinary.com/dqdrsilxz/image/upload/v1636638500/Rome-Italy_isahvk.jpg',price_per_day: 100)
-Destination.create(name: 'Gracier National Park', location: 'USA', image_url: 'https://res.cloudinary.com/dqdrsilxz/image/upload/v1636573679/sample.jpg',price_per_day: 100)
+Destination.create(name: 'Bora Bora', location: 'French Polynesia', image_url: 'https://www.portalmladi.com/wp-content/uploads/2015/10/bora-bora-odmor.png', price_per_day: 150)
+Destination.create(name: 'Rome', location: 'Italy', image_url: 'https://parade.com/.image/t_share/MTkwNTc4Mzg4NTYyODE0ODQ0/colosseum-in-rome-and-morning-sun-italy.jpg',price_per_day: 100)
+Destination.create(name: 'Gracier National Park', location: 'USA', image_url: 'https://www.doi.gov/sites/doi.gov/files/styles/social_media_1200x627/public/blog-post/thumbnail-images/glaciernpshanlin.jpg?itok=BlwMSQH5',price_per_day: 100)
 User.create(username: 'User1', email: 'user@user', password:'123456', password_confirmation:'123456')
 User.create(username: 'Admin', email: 'admin@admin', password:'123456', password_confirmation:'123456', admin: true)
 Reservation.create(user_id: 1, destination_id: 1, startingDay:  "2021-12-01", endingDay: "2021-12-13")
